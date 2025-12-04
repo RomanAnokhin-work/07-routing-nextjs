8eb682200674c52848f08600dd55d19fdc57c765
Roman <roman.anokhin.work@gmail.com>
2025-12-04 15:13:49 +0200

    fix/remove app/notes/page.tsx file

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -1,6 +1,25 @@
-import Notes from "@/app/notes/filter/[...slug]/Notes.client";
+import Notes from "./Notes.client";
+import { fetchNotes } from "@/lib/api";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
 
-const NotesByTagPage = () => {
-  return <Notes />;
-};
+async function NotesByTagPage() {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["notes", 1, ""],
+    queryFn: () => fetchNotes(1, ""),
+  });
+
+  const dehydratedState = dehydrate(queryClient);
+
+  return (
+    <HydrationBoundary state={dehydratedState}>
+      <Notes />
+    </HydrationBoundary>
+  );
+}
 export default NotesByTagPage;
